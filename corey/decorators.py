05c0fcedeaf7d293fb7924b57ce442aa21c19c65
Salmireles <sal.chemicale@gmail.com>
2022-09-28 09:00:27 -0700

    simple decorator

---
+++ corey/decorators.py
@@ -0,0 +1,24 @@
+import functools
+import time
+
+
+def my_simple_decorator(func):
+    @functools.wraps(func)  # fixes original function introspection
+    def wrapper(*args, **kwargs):
+        print("ran first")
+        something = func(*args, **kwargs)
+        print("ran after the function")
+        return something  # in case funciton has a return
+
+    return wrapper
+
+
+def timeit(func):
+    @functools.wraps(func)
+    def time_eval(*args, **kwargs):
+        start_time = time.perf_counter()
+        func(*args, **kwargs)
+        total_time = time.perf_counter() - start_time
+        print(f"Finished {func.__name__!r} in {total_time:.2f} seconds")
+
+    return time_eval
