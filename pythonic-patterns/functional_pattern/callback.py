d471a703a78d2157ff01e333b0372f1a66eb2fd2
Salmireles <sal.chemicale@gmail.com>
2022-10-27 10:47:44 -0700

    functional pattern

---
+++ pythonic-patterns/functional_pattern/callback.py
@@ -0,0 +1,18 @@
+from dataclasses import dataclass
+
+
+@dataclass
+class Button:
+    label: str
+
+    def click(self) -> None:
+        print(f"Clicked on [{self.label}].")
+
+
+def main() -> None:
+    my_button = Button(label="Do Something")
+    my_button.click()
+
+
+if __name__ == "__main__":
+    main()
---
+++ pythonic-patterns/functional_pattern/readme.md
@@ -0,0 +1,4 @@
+# Functional Design Pattern
+
+### Summary
+- Using higher order functions as inputs to functions
